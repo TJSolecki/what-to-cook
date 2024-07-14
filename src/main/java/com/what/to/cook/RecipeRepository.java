1a861c19bea755b34aaeccf883be652efc2a314c
Thomas Solecki <thomasjsolecki@gmail.com>
2024-07-14 11:38:29 -0700

    Refactor RecipeRepository with lombok @Getter

--- src/main/java/com/what/to/cook/RecipeRepository.java
+++ src/main/java/com/what/to/cook/RecipeRepository.java
@@ -8,13 +8,11 @@ import java.util.List;
 
 @Repository
 public class RecipeRepository {
-    private static List<Recipe> recipes = new ArrayList<>();
+    @Getter
+    private static final List<Recipe> recipes = new ArrayList<>();
 
     public void save(Recipe recipe) {
         recipes.add(recipe);
     }
 
-    public static List<Recipe> getRecipes() {
-        return recipes;
-    }
 }
